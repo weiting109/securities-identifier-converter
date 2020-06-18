19f7632d296e9939b22e77718333865fbe0d02bc
Wei-Ting Yap <weitingyp@gmail.com>
2020-06-18 23:21:55 +0800

    Converts Bloomberg tickers to CUSIPs on front-end with specifed format

--- proj/app.py
+++ proj/app.py
@@ -12,12 +12,17 @@ def index():
     """
     Render templates and display tasks according to status
     """
-    return render_template('index.html')
+    return render_template('index.html',to_convert="",converted="")
 
 @app.route('/convert/',methods=['POST'])
 def convert():
-    to_convert=request.form['to-convert']
-    return to_convert
+    to_convert_orig=request.form['to-convert']
+    to_convert = to_convert_orig.split(',')
+    converted=[]
+    for bbticker in to_convert:
+        converted.append(convertBbtoCUSIP(bbticker))
+    converted = ', '.join(converted)
+    return render_template('index.html',to_convert=to_convert_orig,converted=converted)
 
 if __name__ == '__main__':
     app.run(debug=True)

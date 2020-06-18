2ccb0f48e17552ef9fac3cbcd53dfd8f251d221b
Wei-Ting Yap <weitingyp@gmail.com>
2020-06-18 22:12:52 +0800

    Form for Bloomberg ticker submission by 'POST' get method
    Form on proj/templates/index.html using 'POST' method, with
    information sent to new convert endpoint.
    
    Convert endpoint will redirect to index displaying generated
    CUSIPs.

--- proj/app.py
+++ proj/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from converter import convertBbtoCUSIP
 
@@ -7,12 +7,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app) #instantiate SQLAlchemy
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     """
     Render templates and display tasks according to status
     """
     return render_template('index.html')
+
+@app.route('/convert/',methods=['POST'])
+def convert():
+    to_convert=request.form['to-convert']
+    return to_convert
 
 if __name__ == '__main__':
     app.run(debug=True)
